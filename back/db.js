713bf35957080abb234ac73474090ded7744d705
Lívia Santos <142369124+LiviSantos@users.noreply.github.com>
2026-01-19 17:56:43 -0300

    Update database connection settings and error logging

--- back/db.js
+++ back/db.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Silva080167', 
-    database: 'sistema_academico'
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'sistema_academico'
 });
 
-db.connect(err => {
-    if (err) {
-        console.error(' Erro MySQL:', err);
-    } else {
-        console.log(' MySQL conectado');
-    }
+db.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar no banco:', err);
+  } else {
+    console.log('Conectado ao MySQL');
+  }
 });
 
 module.exports = db;
